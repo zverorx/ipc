507b1ad01085ae0dd887d339ce8389b4c340e990
zverorx <zveror1806@gmail.com>
2025-10-12 14:10:51 +0300

    docs: add copyright documentation to each source file

--- src/main.c
+++ src/main.c
@@ -1,7 +1,26 @@
+/* 
+ * This file is part of ipc.
+ * ipc - IP calculator, designed for IP analysis 
+ *
+ * Copyright (C) 2025 Egorov Konstantin
+ *
+ * ipc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ipc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ipc. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /**
  * @file main.c
  * @brief IP calculator, designed for IP analysis
- * @autor: zverorx
  */
 
 #include <stdio.h>
